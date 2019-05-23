824e5d0103141bd8c02cd0c010eb4a2905e3bb99
aobeuang <aob.attaporn@gmail.com>
2019-05-23 19:43:26 +0700

    Update xyz.php

--- xyz.php
+++ xyz.php
@@ -11,37 +11,20 @@ $conn = new mysqli($servername, $username, $password, "heroku_8f3e57c81578b20");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 	} 
-//  $sql = "select date,bid,ask,open from data";
-//  $result = $conn->query($sql);
+ $sql = "select date,bid,ask,open from data";
+ $result = $conn->query($sql);
  
-//  if($result->num_rows>0)//output data of each row
-//  {
-// 	 while($row=$result->fetch_assoc())
-// 	 {
-// 		 echo "date:" .$row["date"]. "bid:" .$row["bid"]. "ask:" .$row["ask"]. "open:" .$row["open"]. "<br>" ;
-// 	 }
-//  }
-//  else
-//  {
-// 	 echo "0 results";
-//  }
-// $conn->close();
+ if($result->num_rows>0)//output data of each row
+ {
+	 while($row=$result->fetch_assoc())
+	 {
+		 echo "date:" .$row["date"]. "bid:" .$row["bid"]. "ask:" .$row["ask"]. "open:" .$row["open"]. "<br>" ;
+	 }
+ }
+ else
+ {
+	 echo "0 results";
+ }
+$conn->close();
 
-
-	$sql = "CREATE TABLE data (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-date VARCHAR(50) NOT NULL,
-bid VARCHAR(50),
-ask VARCHAR(50),
-open VARCHAR(50)
-)";
-
-if (mysqli_query($conn, $sql)) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-echo "555";
 ?>
